d551f1c03d164e0788fa79d1c24a1261584f3ef8
GitHub Action <action@github.com>
2026-05-14 15:45:05 +0530

    Replaced Browser Routes to Hash Routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import axios from "axios";
 import "./App.css";
 import LandingPage from "./components/LandingPage";
