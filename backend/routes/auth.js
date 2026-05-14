955c11efac1edc9762b2445afcdbf10e4c928051
GitHub Action <action@github.com>
2026-05-14 14:42:33 +0530

    Changed bcrypt to bcryptjs due to deployment issues on render

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 // routes/auth.js
 import express from "express";
-import bcrypt from "bcrypt";
+import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
 import User from "../models/User.js";
