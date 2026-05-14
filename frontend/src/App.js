4a4c255eccdc06db9b72f160e343406ebb24dfdc
GitHub Action <action@github.com>
2026-05-14 15:05:01 +0530

    Rectified link of backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,8 @@ function App() {
 
   useEffect(() => {
     axios
-      .get("http://localhost:5000/")
+      // .get("http://localhost:5000/")
+      .get("https://sangeetalaya.onrender.com")
       .then((res) => setMsg(res.data))
       .catch((err) => console.log(err));
   }, []);
